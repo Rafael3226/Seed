6dfe296b1a9aedd7fef9fd90272e98fbcb54d44d
Rafael3226 <rafael-uribe@hotmail.es>
2020-03-10 11:18:10 -0500

    Update querys.js

--- querys.js
+++ querys.js
@@ -9,8 +9,8 @@ db.listings.find({},{"review_scores_rating": true, "name": true}).limit(1).prett
 //    Alojamiento con depósito de seguridad y tarifa de limpieza más baratos (menor x, con x=depósito de seguridad + tarifa de limpieza) 
 
 //    Alojamientos que cuenten con microondas, TV y precio menor a $500 
-
+db.acomodaciones.find({$and: [{amenities: /TV/}, {amenities: /Microwave/}, {price: {$lt: "$500"} }]});
 //    Media de precios de todos los alojamientos que cuenten con microondas 
 db.listings.aggregate([{ $match: {}, {$group: {} } }]);
 //    Alojamientos con menor cantidad de noches mínimas, cantidad máxima de noches mayor a 15, que cuenten con TV y costo menor a $200
-
+db.acomodaciones.find({$and: [{maximum_nights: {$gt: "15"}}, {amenities: /TV/}, {price: {$lt: "$200"}}]}).sort({minimum_nights: 1}) ;
